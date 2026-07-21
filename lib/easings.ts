1c1a31a17b1000751148407070e3e984c37751b2
Haytham <104620852+HaythamSaba@users.noreply.github.com>
2026-07-21 20:52:59 +0200

    deleting unnecessary constant

--- lib/easings.ts
+++ lib/easings.ts
@@ -1,4 +1,3 @@
 // lib/easings.ts
 export const EXPO_OUT = [0.16, 1, 0.3, 1] as [number, number, number, number];
 export const SHARP = [0.76, 0, 0.24, 1] as [number, number, number, number];
-export const SMOOTH = [0.4,  0, 0.2,  1] as [number, number, number, number];
